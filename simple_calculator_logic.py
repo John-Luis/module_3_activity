db3275618d9ea7632c5cba6baa9cd30d9fb26379
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-04-29 19:09:09 +0800

    Importing datetime library and os for later on functionalities on our maangas visual effects, and also created a parent class for handling data storage.. since we will include a feature where the log history of calculation will be stored on a txt file.. applying the previous activity that we did.

--- simple_calculator_logic.py
+++ simple_calculator_logic.py
@@ -1,14 +1,16 @@
+import datetime
+import os
 
 
+#Creating the core math functions here
 def add(a, b): return a + b
-
-
 def sub(a, b): return a - b
-
-
 def mult(a, b): return a * b
-
 
 def div(a, b):
     if b == 0: raise ZeroDivisionError("Cannot divide by zero.")
     return a / b
+
+class FileHandler:
+    def __init__(self, filename):
+        self.filename = filename
