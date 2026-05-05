8b76600b494ff328665a361dce68e0bde36d0933
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-05-06 00:29:45 +0800

    Reorganizing the code of structure, since it was erroring when i run it.

--- simple_calculator_logic.py
+++ simple_calculator_logic.py
@@ -1,5 +1,18 @@
 import datetime
-import os
+
+class FileHandler:
+    def __init__(self, filename="log_history_calculator.txt"):
+        self.filename = filename
+
+    def save_to_vault(self, math_input, result):
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = f"[{timestamp}] INPUT: {math_input} | RESULT: {result}\n"
+        with open(self.filename, "a") as file:
+            file.write(log_entry)
+
+    def wipe_vault(self):
+        """Purges the log file."""
+        open(self.filename, 'w').close()
 
 class CalculatorLogic(FileHandler):
     def __init__(self, filename="log_history_calculator.txt"):
@@ -16,20 +29,4 @@ class CalculatorLogic(FileHandler):
         if b == 0: raise ZeroDivisionError("Cannot divide by zero.")
         return a / b
 
-
-import datetime
-
-class FileHandler:
-    def __init__(self, filename="log_history_calculator.txt"):
-        self.filename = filename
-
-    def save_to_vault(self, math_input, result):
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        log_entry = f"[{timestamp}] INPUT: {math_input} | RESULT: {result}\n"
-        with open(self.filename, "a") as file:
-            file.write(log_entry)
-
-    def wipe_vault(self):
-        """Purges the log file."""
-        open(self.filename, 'w').close()
 
